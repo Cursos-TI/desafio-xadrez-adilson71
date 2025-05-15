0b8bd516661b6b7efef8939c5864059f98f9c33b
adilson <165514801+adilson71@users.noreply.github.com>
2025-05-15 19:24:07 +0000

    recursiva atualizado

--- xadrez.c
+++ xadrez.c
@@ -3,9 +3,9 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+/*
 int main() {
-    /*
+    
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
    int i;                                                                                                          
@@ -177,6 +177,6 @@ void moverTorre(int casaAtual, int totalCasas) {
     // Movimento do Cavalo (com loops complexos)
     moverCavalo();
     printf("======================================\n");
- }
+ 
     return 0;
-}
+ }
